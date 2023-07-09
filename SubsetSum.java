23f2f11941f76cfe9c96a4f42294c051616322ef
Rishikesh pandey <68262598+rishikesh0586@users.noreply.github.com>
2023-07-09 23:34:42 +0530

    Day-26

--- SubsetSum.java
+++ SubsetSum.java
@@ -1,20 +1,33 @@
-import java.util.*;
 public class Solution {
-    public static ArrayList<Integer> subsetSum(int nums[]) {
-        ArrayList<Integer> result = new ArrayList<>();
-        fn(result,nums,nums.length,0,0);
-        Collections.sort(result);
-        return result;
+    static boolean found = false;
+    public static boolean subsetSumToK(int n, int k, int arr[]){
+        int[][] dp = new int[n][k+1];
+        found = false;
+        fn(k,arr,0,dp);
+        return found;
     }
-    public static void fn(ArrayList<Integer> result,int[] nums,int n,int index,int sum){
-        if(n==index){
-            result.add(sum);
+    public static void fn(int k,int[] arr,int index,int[][] dp){
+        if(k==0){
+            found = true;
+            return ;
+        }
+        if(k<0)
             return;
+        if(index==arr.length){
+            return ;
+        }
+        if(dp[index][k]!=0)
+            return;
+        
+        if(!found){
+            fn(k - arr[index],arr,index + 1,dp);
+        }
+        if(!found){
+            fn(k,arr,index + 1,dp);
         }
         
-        fn(result,nums,n,index+1,sum);
-        fn(result,nums,n,index+1,sum+nums[index]);
+        dp[index][k] = -1;
         
         return;
-    }
+    } 
 }
--- WordBreak.java
+++ WordBreak.java
@@ -1,44 +1,42 @@
 import java.util.ArrayList;
-import java.util.*;
+import java.util.List;
 
 public class Solution {
-    public static ArrayList<String> wordBreak(String s, ArrayList<String> dictionary) {
-        ArrayList<String> result = new ArrayList<>();
-        wordBreakHelper(s, dictionary, "", result);
-        return result;
+    public static List<String> getAllValidSentences(String s, String[] dict) {
+        List<String> validSentences = new ArrayList<>();
+        backtrack(s, dict, 0, new StringBuilder(), validSentences);
+        return validSentences;
     }
 
-    private static void wordBreakHelper(String s, ArrayList<String> dictionary, String answer, ArrayList<String> result) {
-        int n = s.length();
+    private static void backtrack(String s, String[] dict, int start, StringBuilder sentence, List<String> validSentences) {
+        if (start == s.length()) {
+            validSentences.add(sentence.toString());
+            return;
+        }
 
-        // Check every prefix one by one
-        for (int i = 1; i <= n; i++) {
-            String prefix = s.substring(0, i);
+        for (int i = start; i < s.length(); i++) {
+            String currentWord = s.substring(start, i + 1);
+            if (isValidWord(currentWord, dict)) {
+                int sentenceLength = sentence.length();
 
-            // If dictionary contains this prefix
-            if (dictionary.contains(prefix)) {
-                // If no more elements are left, add the answer to the result
-                if (i == n) {
-                    result.add(answer + prefix);
-                    return;
+                if (sentenceLength != 0) {
+                    sentence.append(" ");
                 }
+                sentence.append(currentWord);
 
-                // Recursively check for the remaining string
-                wordBreakHelper(s.substring(i, n), dictionary, answer + prefix + " ", result);
+                backtrack(s, dict, i + 1, sentence, validSentences);
+
+                sentence.setLength(sentenceLength); // backtrack by removing the currentWord
             }
         }
     }
 
-    public static void main(String[] args) {
-        String input = "Ihavewatchedthenewspider-manmovieinthetheater";
-        ArrayList<String> dictionary = new ArrayList<>(Arrays.asList("apple", "devices", "love", "I", "have", "the", "new", "spider-man", "movie", "watched", "in", "theater"));
-
-        ArrayList<String> result = wordBreak(input, dictionary);
-
-        System.out.println("Input: " + input);
-        System.out.println("Output: ");
-        for (String word : result) {
-            System.out.println(word);
+    private static boolean isValidWord(String word, String[] dict) {
+        for (String validWord : dict) {
+            if (validWord.equals(word)) {
+                return true;
+            }
         }
+        return false;
     }
 }
